8a596cc8a3c5b114eb3bf1f35ebe1848e7190c6e
dunissimo <SuperDen214@yandex.ru>
2024-08-20 20:18:51 +0400

    fix: getData

--- api/getData.ts
+++ api/getData.ts
@@ -2,15 +2,7 @@ import { API_URL } from "@/utils/consts";
 import { IData } from "@/utils/types";
 
 export const getData = async (): Promise<IData> => {
-    if (process.env.NODE_ENV === "development") {
-        return getDataFromNpoint();
-    }
-
-    return getDataFromNpoint("https://api.npoint.io/2333dc1e1d95de6b56a5");
-};
-
-const getDataFromNpoint = async (url?: string) => {
-    const data = await fetch(url || API_URL || "");
+    const data = await fetch(API_URL || "");
 
     return data.json();
 };
